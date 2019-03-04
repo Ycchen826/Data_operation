a541a3f589aa8d1c0c5b92908b2a9122c740a4b0
Ycchen826 <yc_chen826@163.com>
2019-03-04 10:47:51 +0800

    normal update

--- data_augmentation.py
+++ data_augmentation.py
@@ -6,15 +6,25 @@
 # 导入库
 import numpy as np
 import cv2
+from  PIL import Image as Img
+
 #伽马矫正
 def gamma_trans(img, gamma):
     gamma_table = [np.power(x / 255.0, gamma)*255.0 for x in range(256)]
     gamma_table = np.round(np.array(gamma_table)).astype(np.uint8)
     return cv2.LUT(img, gamma_table)
 
+"""path_in和path_out都是路径名加文件名（含后缀）如F:\Cam1.png, rate是压缩的比率"""
+def compress(path_in,path_out,rate):
+        # 打开图片
+        image = Img.open(path_in)
+        # 图片另存到output文件夹中，图片质量压缩到60%
+        image.save(path_out, quality=rate)
+
 def augmentation():
     
     img = cv2.imread('F:\Cam1.png')
+        
     cv2.imshow('src',img)
     
     
